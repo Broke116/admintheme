782fb7a0421eb3cb4b11b50ea366637874d95de5
Ekin Yücel <Broke116@users.noreply.github.com>
2015-08-25 17:51:05 +0300

    morris.js additions

--- js/App.js
+++ js/App.js
@@ -18,6 +18,7 @@ app.controller('chartCtrl', ['$scope', function ($scope) {
     };
 } ]);
 
+// like , sub , cust , ord line graphs
 $(function () {
     var myvalues = [1, 8, 5, 7, 4, 4, 9];
     $('.inlinebarlike').sparkline(myvalues, { type: 'bar', barColor: 'orange' }); // bar chart sparkline eklentisi
@@ -26,29 +27,42 @@ $(function () {
     $('.inlinebarord').sparkline(myvalues, { type: 'bar', barColor: '#f68484' });
 });
 
+var visitordata = [
+                    { y: '2009', a: 750, b: 250, c: 260 },
+                    { y: '2010', a: 500, b: 120, c: 150 },
+                    { y: '2011', a: 600, b: 40, c: 80 },
+                    { y: '2012', a: 650, b: 160, c: 300 },
+                    { y: '2013', a: 625, b: 40, c: 350 },
+                    { y: '2014', a: 725, b: 180, c: 210 },
+                    { y: '2015', a: 650, b: 100, c: 140 }
+                 ];
+
 Morris.Line({ // morris.js line chart
     element: 'linechartuser',
-    data: [
-    { y: '2009', a: 750, b: 250, c: 260 },
-    { y: '2010', a: 500, b: 120, c: 150 },
-    { y: '2011', a: 600, b: 40, c: 80 },
-    { y: '2012', a: 650, b: 160, c: 300 },
-    { y: '2013', a: 625, b: 40, c: 350 },
-    { y: '2014', a: 725, b: 180, c: 210 },
-    { y: '2015', a: 650, b: 100, c: 140 }
-  ],
+    data: visitordata,
+    hideHover: 'auto',
     xkey: 'y',
     ykeys: ['a', 'b', 'c'],
     labels: ['View Count', 'Unique Views', 'User Count']
 });
 
+var browserdata = [
+                    { label: "Safari", value: 12 },
+                    { label: "Chrome", value: 40 },
+                    { label: "Firefox", value: 35 },
+                    { label: "IE", value: 5 },
+                    { label: "Others", value: 8 }
+                 ];
+
 Morris.Donut({ // morris.js donut chart
     element: 'donutbrowser',
-    data: [
-    { label: "Safari", value: 12 },
-    { label: "Chrome", value: 40 },
-    { label: "Firefox", value: 35 },
-    { label: "IE", value: 5 },
-    { label: "Others", value: 8 }
-  ]
+    data: browserdata
 });
+
+//$.getJSON("data.json", function (data) {
+//    Morris.Donut({
+//        element: "donutbrowser",
+//        data: $.parseJSON(data),
+//        labels: ["Count"]
+//    });
+//});
